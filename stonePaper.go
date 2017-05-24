4fc949e3e7e41c06f8877da39491394e145cd4d5
Matthew Rappard <mrappard@gmail.com>
2017-05-24 14:09:46 -0400

    added prototype  working

--- stonePaper.go
+++ stonePaper.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
 	"strconv"
-	"strings"
 	"time"
-
+	"errors"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
-	pb "github.com/hyperledger/fabric/protos/peer"
+
 )
 
 
@@ -22,12 +19,16 @@ type stonePaper struct {
 	ContractType  int `json:"contractType"`
 }
 
+// StonePaperChaincode
+// ===========================
+type StonePaperChaincode struct {
+}
 
 // ===================================================================================
 // Main
 // ===================================================================================
 func main() {
-	err := shim.Start(new(SimpleChaincode))
+	err := shim.Start(new(StonePaperChaincode))
 	if err != nil {
 		fmt.Printf("Error starting Simple chaincode: %s", err)
 	}
@@ -35,14 +36,13 @@ func main() {
 
 // Init initializes chaincode
 // ===========================
-func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
-	return shim.Success(nil)
+func (t *StonePaperChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	return nil, nil
 }
 
 // Invoke - Our entry point for Invocations
 // ========================================
-func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
-	function, args := stub.GetFunctionAndParameters()
+func (t *StonePaperChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	fmt.Println("invoke is running " + function)
 
 	// Handle different functions
@@ -52,62 +52,97 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
 		return t.setDatabase(stub, args)
 	} else if function == "setName" { //transfer all marbles of a certain color
 		return t.setName(stub, args)
-	} else if function == "getDoc"{
-		return t.getDoc(stub, args)
 	}
 	fmt.Println("invoke did not find func: " + function) //error
-	return shim.Error("Received unknown function invocation")
+	return nil, errors.New("Received unknown function invocation")
 }
+
+
+
+
+// Query callback representing the query of a chaincode
+func (t *StonePaperChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	if function != "getDoc" {
+		return nil, errors.New("Invalid query function name. Expecting \"query\"")
+	}
+	var A string // Entities
+	var err error
+
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting Hash of document")
+	}
+
+	A = args[0]
+
+	// Get the state from the ledger
+	Avalbytes, err := stub.GetState(A)
+	if err != nil {
+		jsonResp := "{\"Error\":\"Failed to get document for " + A + "\"}"
+		return nil, errors.New(jsonResp)
+	}
+
+	if Avalbytes == nil {
+		jsonResp := "{\"Error\":\"No document for " + A + " in Blockchain\"}"
+		return nil, errors.New(jsonResp)
+	}
+
+	//jsonResp := "{\"Name\":\"" + A + "\",\"Amount\":\"" + string(Avalbytes) + "\"}"
+	//fmt.Printf("Query Response:%s\n", jsonResp)
+	return Avalbytes, nil
+}
+
+
+
 
 
 // ============================================================
 // initMarble - create a new marble, store into chaincode state
 // ============================================================
-func (t *SimpleChaincode) createDoc(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+func (t *StonePaperChaincode) createDoc(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
 	var err error
 	//   0       	1       		2     					3
 	// "DocHash", "Database", "SubContract", "ContractType"
 	if len(args) != 4 {
-		return shim.Error("Incorrect number of arguments. Expecting 4")
+		return nil, errors.New("Incorrect number of arguments. Expecting 4")
 	}
 
 	// ==== Input sanitation ====
 	fmt.Println("- start init marble")
 	if len(args[0]) <= 0 {
-		return shim.Error("1st argument must be a non-empty string")
+		return nil, errors.New("1st argument must be a non-empty string")
 	}
 	if len(args[1]) <= 0 {
-		return shim.Error("2nd argument must be a non-empty string")
+		return nil, errors.New("2nd argument must be a non-empty string")
 	}
 	if len(args[2]) <= 0 {
-		return shim.Error("3rd argument must be a non-empty string")
+		return nil, errors.New("3rd argument must be a non-empty string")
 	}
 	if len(args[3]) <= 0 {
-		return shim.Error("4th argument must be a non-empty string")
+		return nil, errors.New("4th argument must be a non-empty string")
 	}
 
 	DocHash := args[0]
 	Database, err := strconv.Atoi(args[1])
 	if err != nil {
-		return shim.Error("2rd argument must be a numeric string")
+		return nil, errors.New("2rd argument must be a numeric string")
 	}
 	SubContract := args[2]
 	ContractType, err := strconv.Atoi(args[3])
 	if err != nil {
-		return shim.Error("4rd argument must be a numeric string")
+		return nil, errors.New("4rd argument must be a numeric string")
 	}
-	t := time.Now()
-	TimeV := t.String()
-	Creator := shim.GetCreator()
+	timerValue := time.Now()
+	TimeV := timerValue.String()
+	Creator := "Test"//stub.GetCreator()
 
 
 	// ==== Check if doc with matching hash exists already exists ====
 	docAsBytes, err := stub.GetState(DocHash)
 	if err != nil {
-		return shim.Error("Failed to get doc: " + err.Error())
+		return nil, errors.New("Failed to get doc: " + err.Error())
 	} else if docAsBytes != nil {
 		fmt.Println("This Hash already exists: " + DocHash)
-		return shim.Error("This Hash already exists: " + DocHash)
+		return nil, errors.New("This Hash already exists: " + DocHash)
 	}
 
 /*
@@ -116,12 +151,12 @@ func (t *SimpleChaincode) createDoc(stub shim.ChaincodeStubInterface, args []str
 	marble := &stonePaper{DocHash, Database, TimeV, Creator, SubContract, ContractType}
 	marbleJSONasBytes, err := json.Marshal(marble)
 	if err != nil {
-		return shim.Error(err.Error())
+		return nil, errors.New(err.Error())
 	}
 	*/
 
-	stonePaperJSONasString := `{"DocHash":"`DocHash`","Database": "`+Database+`", "Time":"`+TimeV+`","Creator":"`+Creator+`", "SubContract": "`+SubContract+`","ContractType": "`+ContractType+`"}`
-	stonePaperJSONasBytes := []byte(str)
+	stonePaperJSONasString := `{"DocHash":"`+DocHash+`","Database":`+strconv.Itoa(Database)+`,"Time":"`+TimeV+`","Creator":"`+Creator+`","SubContract": "`+SubContract+`","ContractType":`+strconv.Itoa(ContractType)+`}`
+	stonePaperJSONasBytes := []byte(stonePaperJSONasString)
 
 	//Alternatively, build the marble json string manually if you don't want to use struct marshalling
 	//marbleJSONasString := `{"docType":"Marble",  "name": "` + marbleName + `", "color": "` + color + `", "size": ` + strconv.Itoa(size) + `, "owner": "` + owner + `"}`
@@ -130,35 +165,17 @@ func (t *SimpleChaincode) createDoc(stub shim.ChaincodeStubInterface, args []str
 	// === Save marble to state ===
 	err = stub.PutState(DocHash, stonePaperJSONasBytes)
 	if err != nil {
-		return shim.Error(err.Error())
+		return nil, errors.New(err.Error())
 	}
 	// ==== Marble saved and indexed. Return success ====
 	fmt.Println("- end init document")
-	return shim.Success(nil)
+	return nil, err
 }
 
-func (t *SimpleChaincode) setDatabase(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	return shim.Success(nil)
+func (t *StonePaperChaincode) setDatabase(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	return nil, nil
 }
 
-func (t *SimpleChaincode) setName(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	return shim.Success(nil)
-}
-
-func (t *SimpleChaincode) getDoc(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	//   0
-	// "DocHash"
-	if len(args) < 1 {
-		return shim.Error("Incorrect number of arguments. Expecting 1")
-	}
-
-	DocHash := args[0]
-
-	queryString := fmt.Sprintf("{\"selector\":{\"docType\":\"stonePaper\",\"DocHash\":\"%s\"}}", DocHash)
-
-	queryResults, err := getQueryResultForQueryString(stub, queryString)
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-	return shim.Success(queryResults)
+func (t *StonePaperChaincode) setName(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	return nil, nil
 }
